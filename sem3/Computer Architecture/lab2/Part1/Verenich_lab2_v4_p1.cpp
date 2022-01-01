b2ad452c266de708bb09874aa16ff06f0ba222e6
Влад Веренич <vverchickktv@gmail.com>
2022-01-01 15:59:41 +0300

    add a few more subjects

---
+++ sem3/Computer Architecture/lab2/Part1/Verenich_lab2_v4_p1.cpp
@@ -0,0 +1,84 @@
+﻿#include <omp.h>
+#include <iostream>
+#include <chrono>
+
+class Timer
+{
+private:
+	using clock_t = std::chrono::high_resolution_clock;
+	using second_t = std::chrono::duration<double, std::ratio<1> >;
+
+	std::chrono::time_point<clock_t> m_beg;
+
+public:
+	Timer() : m_beg(clock_t::now())
+	{
+	}
+
+	void reset()
+	{
+		m_beg = clock_t::now();
+	}
+
+	double elapsed() const
+	{
+		return std::chrono::duration_cast<second_t>(clock_t::now() - m_beg).count();
+	}
+};
+
+const double PI = 3.141592653589793238463;
+
+void getMessageAboutWrongInput() {
+	std::cout << "\nPlease enter a valid number!\n";
+	std::cin.clear();
+	std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+}
+
+double functionF(const double x) {
+	if (x >= -PI / 2 && x <= PI / 2) {
+		return sin(x) - x;
+	}
+	return -1;
+}
+
+void getMaxSecondDerivative(const int64_t N, double &result) {
+	omp_set_num_threads(5);
+
+	double a{ -PI / 2 };
+	double b = { PI / 2 };
+	double h = { (b - a) / N };
+	result = 0;
+
+#pragma omp parallel for schedule(static,1) ordered
+	for (int i{ 1 }; i < N; ++i) {
+		double f = (functionF(a + h * (i - 1)) - 2 * functionF(a + h * i) + functionF(a + h * (i + 1))) / (h * h);
+
+		if (std::abs(f) > result) {
+			result = std::abs(f);
+		}
+	}
+}
+
+double getCalculatedMaxSecondDerivative() {
+	return 1;
+}
+
+int main() {
+	int64_t N;
+	for (;;) {
+		std::cout << "Please enter N: ";
+		if (std::cin >> N && N > 1) {
+			break;
+		} else {
+			getMessageAboutWrongInput();
+		}
+	}
+	
+	Timer timer;
+	double result;
+	getMaxSecondDerivative(N, result);
+
+	std::cout << "\nAbsolute max value of second derivative, calculated using several threads, equals to " << result;
+	std::cout << "\nReal absolute max value of second derivative equals to " << getCalculatedMaxSecondDerivative();
+	std::cout << "\nElapsed: " << timer.elapsed() << " s";
+}
---
+++ sem3/Computer Architecture/lab2/Part2/Verenich_lab2_v4_p2.cpp
@@ -0,0 +1,130 @@
+﻿#include <omp.h>
+#include <iostream>
+#include <chrono>
+
+class Timer
+{
+private:
+	using clock_t = std::chrono::high_resolution_clock;
+	using second_t = std::chrono::duration<double, std::ratio<1> >;
+
+	std::chrono::time_point<clock_t> m_beg;
+
+public:
+	Timer() : m_beg(clock_t::now())
+	{
+	}
+
+	void reset()
+	{
+		m_beg = clock_t::now();
+	}
+
+	double elapsed() const
+	{
+		return std::chrono::duration_cast<second_t>(clock_t::now() - m_beg).count();
+	}
+};
+
+const double PI = 3.141592653589793238463;
+
+void getMessageAboutWrongInput() {
+	std::cout << "\nPlease enter a valid number!\n";
+	std::cin.clear();
+	std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+}
+
+double functionF(double x, const double a, const double b) {
+	if (x >= a && x <= 1) {
+		return 2 * x - 2;
+	} else if (x > 1 && x <= b) {
+		return sin(x - 1);
+	}
+	return -1;
+}
+
+void calculateIntegralSimpson(int64_t N, double a) {
+	omp_set_num_threads(10);
+	double resultForN{ 0 };
+	double resultFor4N{ 0 };
+	double b{ 1 + PI / 2 };
+	double hN{ (b - a) / N };
+	double h4N{ (b - a) / (4 * N) };
+	//std::cout << h << " " << b << "\n";
+#pragma omp parallel sections
+	{
+#pragma omp section
+		{
+#pragma omp parallel for schedule(static,1) ordered
+			for (int i{ 0 }; i < N + 1; ++i) {
+				if (i != 0 && i != N - 1) {
+					if (i % 2 == 1) {
+						resultForN += (hN / 3) * functionF(a + i * hN, a, b) * 4;
+					} else {
+						resultForN += (hN / 3) * functionF(a + i * hN, a, b) * 2;
+					}
+				} else {
+					resultForN += (hN / 3) * functionF(a + i * hN, a, b);
+				}
+			}
+		}
+#pragma omp section 
+		{
+#pragma omp parallel for schedule(static,1) ordered
+			for (int i{ 0 }; i < 4 * N + 1; ++i) {
+				if (i != 0 && i != 4 * N - 1) {
+					if (i % 2 == 1) {
+						resultFor4N += (h4N / 3) * functionF(a + i * h4N, a, b) * 4;
+					} else {
+						resultFor4N += (h4N / 3) * functionF(a + i * h4N, a, b) * 2;
+					}
+				} else {
+					resultFor4N += (h4N / 3) * functionF(a + i * h4N, a, b);
+				}
+			}
+		}
+	}
+	std::cout << "\nIntegral, calculated using Simpson's method (N), equals to " << resultForN;
+	std::cout << "\nIntegral, calculated using Simpson's method (4*N), equals to " << resultFor4N;
+}
+
+double calculateIntegral(double a) {
+	auto lambda1{ [](double x) { //indefinite integral of f1(x) = 2*x-2
+		return x * x - 2 * x;
+	} };
+
+	auto lambda2{ [](double x) {//indefinite integral of f2(x) = sin(x-1)
+		return -cos(x - 1);
+	} };
+
+	return lambda1(1) - lambda1(a) + lambda2(1 + PI / 2) - lambda2(1);
+}
+
+int main() {
+	int64_t N;
+	for (;;) {
+		std::cout << "Please enter N: ";
+		if (std::cin >> N && N > 0) {
+			break;
+		} else {
+			getMessageAboutWrongInput();
+		}
+	}
+
+	double a;
+	for (;;) {
+		std::cout << "Please enter a: ";
+		if (std::cin >> a && a < 1) {
+			break;
+		} else {
+			getMessageAboutWrongInput();
+		}
+	}
+
+	Timer timer;
+	double result;
+	calculateIntegralSimpson(N, a);
+
+	std::cout << "\nIntegral, calculated using RTL, equals to " << calculateIntegral(a);
+	std::cout << "\nElapsed: " << timer.elapsed() << " s";
+}
